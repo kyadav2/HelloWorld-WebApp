662e397fc0f6cd40118bca113df938310a70f65d
Kishor Yadav <kyadav@practicefusion.com>
2020-02-11 11:13:28 -0800

    Removed commented code

--- src/Host/Startup.cs
+++ src/Host/Startup.cs
@@ -24,28 +24,12 @@ namespace PF.PfCoreHelloWorld
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddPFCoreWebApplicationServices(HostingEnvironment);
-            //services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UsePFCoreRequestPipeline(HostingEnvironment);
-            /*
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-            */
         }
     }
 }
